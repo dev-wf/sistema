6ebff4fba72f289cd39ed4ac1b7efa8e7d16ecef
dev-wf <w.a.f@outlook.com.br>
2022-08-07 00:17:12 -0300

    update

--- js/load.js
+++ js/load.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
 
     if (counter == 101) {
       clearInterval(i);
-      window.location.href = "../main.html";
+      window.location.href = "sistema/main.html";
     }
   }, 50);
 });
