cee2a282cfecfeb532a2b99b18e29be9c3c9691a
dev-wf <w.a.f@outlook.com.br>
2022-08-07 19:05:25 -0300

    update

--- js/main.js
+++ js/main.js
@@ -2,12 +2,11 @@ function typeEscrita(elemento) {
     var textArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
     textArray.forEach((letra, i) => {
-        setTimeout(() => {
-            elemento.innerHTML += letra;
-        }, 50 * i)
-    })
+        setTimeout(() =>
+            elemento.innerHTML += letra, 50 * i)
+    });
 
 }
-var anime = document.querySelector("h5");
+var anime = document.querySelector("p");
 typeEscrita(anime);
 
