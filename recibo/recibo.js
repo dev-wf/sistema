1b3f962c9f6ef942a5a95074fead7444c52e84d9
dev-wf <w.a.f@outlook.com.br>
2022-08-11 00:21:45 -0300

    update

--- recibo/recibo.js
+++ recibo/recibo.js
@@ -16,41 +16,5 @@ $(document).on('keydown', '[data-mask-for-cpf-cnpj]', function (e) {
 //MASCARA DINHEIRO:
 $("#valProduto").maskMoney();
 
-//DATA ATUAL
-const date = new Date().toLocaleDateString();
-// BOTAO EMITIR NOTA:
-$().click(() => {
-    let data = $('#data').val().split('-').reverse().join('/');
-    if (data == '') {
-        console.log(date);
-    } else {
-        data
-    }
 
-
-    let nome = $('#clinome').val();
-    let cliDoc = $('#clidoc').val();
-
-    let produto = $('#produto').val();
-    let valProduto = $('#valProduto').val();
-
-    let eminome = $('#eminome').val();
-    let emiDoc = $('#emidoc').val();
-    if (eminome == '' && emiDoc == '') {
-        eminome = "WF TECH-Developer";
-        emiDoc = "43.652.172/0001-52";
-    } else {
-        eminome;
-        emiDoc
-    }
-
-    console.log(data);
-    console.log(nome);
-    console.log(cliDoc);
-    console.log(produto);
-    console.log(valProduto);
-    console.log(eminome);
-    console.log(emiDoc);
-
-})
 
